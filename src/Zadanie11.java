e01d4a3b14a9867b719bbc78dc2c22e77419e2d1
KWPiotr <889786700Pi0tr>
2021-10-24 23:19:24 +0200

    Zadanie 11 - Wyszukiwanie najdłuższego łańcucha zankowego (wersja ostateczna).

--- src/Zadanie11.java
+++ src/Zadanie11.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class Zadanie11 {
     public static void main(String[] args) {
-        System.out.println("Wprowadzaj napisy (Starczy kończy proces wprowadzania): ");
+        System.out.println("Wprowadzaj napisy (słowo \"Starczy\" - kończy proces wprowadzania): ");
         Scanner strUser = new Scanner(System.in);
         ArrayList<String> userData = new ArrayList<>();
+
         int index = 0;
         int counterChar;
         int counterTmp = 0;
@@ -13,11 +14,11 @@ public class Zadanie11 {
         do {
             userData.add(strUser.nextLine());
 
-            if(userData.get(index).isEmpty()) {
+            if (userData.get(index).isEmpty()) {
                 System.out.println("!!! Nie podano żadnego łancucha znakowego !!!");
             }
 
-            if(userData.get(index).equals("Starczy")) {
+            if (userData.get(index).equals("Starczy")) {
                 break;
             } else {
                 counterChar = userData.get(index).length();
@@ -28,13 +29,12 @@ public class Zadanie11 {
                 }
                 ++index;
             }
-        } while(!userData.get(index-1).equals("Starczy"));  //podwójnie zabezpieczone
+        } while (!userData.get(index - 1).equals("Starczy"));  //podwójnie zabezpieczone
 
         System.out.println("Najdłuższy ciąg znaków wynosi: " + counterTmp);
 
-        for(int i = 0; i < userData.size(); i++) {
-            if(userData.get(i).length() == counterTmp && !userData.get(i).equals("Starczy"))
-            {
+        for (int i = 0; i < userData.size(); i++) {
+            if (userData.get(i).length() == counterTmp && !userData.get(i).equals("Starczy")) {
                 System.out.println("Najdłuższym łańcuchem znakowym okazał się: " + userData.get(i));
             }
         }
