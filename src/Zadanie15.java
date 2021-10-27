fc68ee47a87f50fb0143bdf9c3c45e8efb6538c6
KWPiotr <piotr.kwapisz@gmail.com>
2021-10-27 09:09:20 +0200

    Zadanie 15 - wyświetlanie liczby występujących conajmniej 2 razy (wymaga jeszce dopracowania)

---
+++ src/Zadanie15.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Zadanie15 {
+    public static void main(String[] args) {
+        System.out.println("Podaj 10 liczb: ");
+        Scanner strUser = new Scanner(System.in);
+        int[] userIint = new int[10];
+        int tmp;
+        for (int i = 0; i < 10; i++) {
+            userIint[i] = strUser.nextInt();
+        }
+        for (int i = 0; i < userIint.length; i++) {
+            tmp = szukaj(userIint, userIint[i]);
+            if(tmp!=0) {
+                System.out.println(tmp);
+            }
+        }
+
+    }
+    static int szukaj(int[] inArray, int inputNumber) {
+        int counter = 0;
+        for (int i = 0; i < inArray.length; i++) {
+            if(inArray[i] == inputNumber) {
+                ++counter;
+            }
+        }
+
+        if(counter >= 2) {
+            return inputNumber;
+        }
+        return 0;
+    }
+}
