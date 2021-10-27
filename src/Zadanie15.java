8ad21457d2f579b54f2e23b108b3f4b93c77c1b1
KWPiotr <piotr.kwapisz@gmail.com>
2021-10-27 21:48:29 +0200

    Zadanie 15 - wyświetlanie liczby występujących conajmniej 2 razy (naprawiona funkcjonalność)

--- src/Zadanie15.java
+++ src/Zadanie15.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Zadanie15 {
@@ -5,30 +6,37 @@ public class Zadanie15 {
         System.out.println("Podaj 10 liczb: ");
         Scanner strUser = new Scanner(System.in);
         int[] userIint = new int[10];
-        int tmp;
+
         for (int i = 0; i < 10; i++) {
             userIint[i] = strUser.nextInt();
         }
-        for (int i = 0; i < userIint.length; i++) {
-            tmp = szukaj(userIint, userIint[i]);
-            if(tmp!=0) {
-                System.out.println(tmp);
-            }
-        }
+
+        szukaj(userIint);
         strUser.close();
     }
 
-    static int szukaj(int[] inArray, int inputNumber) {
-        int counter = 0;
+    static void szukaj(int[] inArray) {
+        int counter;
+        Arrays.sort(inArray);
+//        for (int i = 0; i < inArray.length; i++) {
+//            System.out.println(inArray[i]);
+//        }
+//        System.out.println("-----------------------------");
         for (int i = 0; i < inArray.length; i++) {
-            if(inArray[i] == inputNumber) {
-                ++counter;
+            counter = 0;
+            for (int j = i; j < inArray.length; j++) {
+                if (inArray[i] == inArray[j]) {
+                    ++counter;
+
+                    if (counter >= 2) {
+                        ++i;
+                        continue;
+                    }
+                }
+            }
+            if (counter >= 2) {
+                System.out.println(inArray[i]);
             }
         }
-
-        if(counter >= 2) {
-            return inputNumber;
-        }
-        return 0;
     }
 }
