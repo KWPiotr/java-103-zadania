e6e39f423e9eba90cc72c037bdfb64c68bc6c8f4
KWPiotr <piotr.kwapisz@gmail.com>
2021-10-29 22:44:34 +0200

    Zadanie 20 - Typowanie wylosowanych liczb przez generator Random.nextInt()

---
+++ src/Zadanie20.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zadanie20 {
+    public static void main(String[] args) {
+
+        Random varNumber = new Random();
+        int generateNumber =  varNumber.nextInt(100);
+        System.out.println("Wygenerowana liczna, to: " + generateNumber);
+
+        System.out.print("Podaj liczbę ");
+        Scanner strUser = new Scanner(System.in);
+        int myNumber;
+
+        while (true) {
+            myNumber = strUser.nextInt();
+
+            if(myNumber > generateNumber) {
+                System.out.println("Za dużo");
+                continue;
+            } else if(myNumber < generateNumber) {
+                System.out.println("Za mało");
+                continue;
+            } else if(myNumber == generateNumber) {
+                System.out.println("Bingo");
+                break;
+            }
+
+        }
+    }
+}
